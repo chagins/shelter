47e964658fa3d4323e2d8910b8d7a42531597f3f
chagins <chagin_yav@hotmail.com>
2022-04-18 07:34:38 +0500

    feat: adaptive our-pets-320

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -2,9 +2,11 @@
 
 const menu_list = document.querySelector('.menu-list');
 const menu_links = document.querySelectorAll('.menu-link');
+const burger = document.querySelector('.burger');
 
 if(menu_list) {
   menu_list.addEventListener('click', e => {
+    openMenu(e);
     if(menu_links.length > 0) {
       menu_links.forEach(menu_link => {
         menu_link.classList.remove('active');
@@ -13,3 +15,12 @@ if(menu_list) {
     e.target.classList.add('active');
   })
 };
+
+if(burger) {
+  burger.addEventListener('click', openMenu);
+}
+
+function openMenu() {
+  burger.classList.toggle('active');
+  menu_list.classList.toggle('active');
+}
