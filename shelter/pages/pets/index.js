aa042f824f5d1db851b8004a1308d10de0cc32ad
chagins <chagin_yav@hotmail.com>
2022-04-25 06:47:34 +0500

    feat: add dimmer for burger menu

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -3,6 +3,8 @@
 const menu_list = document.querySelector('.menu-list');
 const menu_links = document.querySelectorAll('.menu-link');
 const burger = document.querySelector('.burger');
+const logo = document.querySelector('.logo');
+const dimmer = document.querySelector('.dimmer');
 
 if(menu_list) {
   menu_list.addEventListener('click', e => {
@@ -20,7 +22,14 @@ if(burger) {
   burger.addEventListener('click', openMenu);
 }
 
+if(dimmer) {
+  dimmer.addEventListener('click', openMenu);
+}
+
 function openMenu() {
   burger.classList.toggle('active');
   menu_list.classList.toggle('active');
+  logo.classList.toggle('menu-open');
+  dimmer.classList.toggle('active');
+  document.body.classList.toggle('lock');
 }
